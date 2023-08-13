48cbdd646534f685380ed993de5961f4890e927f
Zamelane <zamelane@vk.com>
2023-08-13 15:54:50 +0700

    Добавлен функционал для возврата превью картинок

--- api/functions/folders.php
+++ api/functions/folders.php
@@ -12,11 +12,12 @@ function getFolder() {
     $result_path        = checkIndex($index_path_check, $folder);                                  //    | Если индекс существует, то записываем
     $global_path        = $config['path'];                                                        //     | Итоговый путь для сканирования
     $include_files      = getPost('includeFiles', 'no') == 'yes' ? true : false;                 //      | Включать ли в выдаче файлы ?
-    $directories        = scandir($global_path . '\\' . $result_path);                          //       | Сканируем дирректорию
-    $return             = getBody(true);                                                       //        | Подготавливаем шаблон успешного ответа
-    $page               = getPost('page', '1');                                               //         | Номер страницы для отображения
-    $elementsOfPage     = getPost('elementsOfPage', $config['elementsOfPage']);              //          | Количество элементов на странице
-    $visibleElements    = 0;                                                                //           | Количество отображённых элементов
+    $resolution         = checkResolution(getPost('resolution', 'no'));                         //       | Размер отдаваемых превьюшек
+    $directories        = scandir($global_path . '\\' . $result_path);                         //        | Сканируем дирректорию
+    $return             = getBody(true);                                                      //         | Подготавливаем шаблон успешного ответа
+    $page               = getPost('page', '1');                                              //          | Номер страницы для отображения
+    $elementsOfPage     = getPost('elementsOfPage', $config['elementsOfPage']);             //           | Количество элементов на странице
+    $visibleElements    = 0;                                                               //            | Количество отображённых элементов
 
                         $return['data']['folders'] = [];
     if ($include_files) $return['data'][ 'files' ] = [];
@@ -38,12 +39,30 @@ function getFolder() {
             } else if ($include_files) {
                 if (exif_imagetype($this_folder_path)) {
                     $visibleElements++;
-                    if (($page - 1) * $elementsOfPage < $visibleElements && $visibleElements <= $page * $elementsOfPage)
-                    array_push($return['data']['files'], [
-                        'name'      => $value,
-                        'path'      => $this_folder_path,
-                        'imageType' => exif_imagetype($this_folder_path)
-                    ]);
+                    if (($page - 1) * $elementsOfPage < $visibleElements && $visibleElements <= $page * $elementsOfPage) {
+                        $push_data = [                                                                          // Подготавливаем ответ
+                            'name'      => $value,
+                            'path'      => $this_folder_path,
+                            'imageType' => exif_imagetype($this_folder_path)
+                        ];
+                        
+                        if ($resolution != 0) {
+                            $name    = md5($this_folder_path) . '.' . pathinfo($this_folder_path, PATHINFO_EXTENSION);
+                            $catalog = dirname(__DIR__, 1) . '\\cache\\w' . $resolution;
+                            if (!file_exists($catalog)) mkdir($catalog);
+                            $cache_path = $catalog . '\\' . $name;
+                            if (!file_exists($cache_path)) {
+                                include_once "thumbs.php";                                                      // Подключаем обработчик миниатюр
+                                $image = new Thumbs($this_folder_path);
+                                $image->resize($resolution, 0);
+                                $image->save($cache_path);
+                            }
+                            $push_data['resolution'] = 'w'.$resolution;
+                            $push_data['w'.$resolution] = 'http://' . $_SERVER['SERVER_NAME'] . '/api/cache/w' . $resolution. '/' . $name;
+                        }
+                        
+                        array_push($return['data']['files'], $push_data);                                       // Пушим ответ
+                    }
                 }
             }
         }
@@ -59,6 +78,16 @@ function checkIndex($checkIndex, $folder) {
     } else {                                                                            // В ином случае выбрасываем ошибку
         returnBody(getBody(false, '2', 'Указанный индекс не найден!'));
         exit();
+    }
+}
+function checkResolution($resolution) {
+    switch ($resolution) {
+        case "w400" : return 400 ;
+        case "w600" : return 600 ;
+        case "w800" : return 800 ;
+        case "w1000": return 1000;
+        case "w1200": return 1200;
+        default     : return 0   ;
     }
 }
 ?>
